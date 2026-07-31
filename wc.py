233fb7ef0ea1d3465201c37adff6da91a947f329
alireza200411 <alirezamoradi292048@gmail.com>
2026-07-31 20:05:25 +0330

    implement [wc -m], [wc -c]

--- wc.py
+++ wc.py
@@ -6,20 +6,23 @@ def main():
     modes = {
         '-l': 'line',
         '-c': 'bytes', 
-        '-w': 'word'
+        '-w': 'word',
+        '-m': 'char',
+        '-L': 'long line'
     }
+    if len(argv) < 2:
+        raise IOError('')
     try:
         if not path.isfile(argv[1]):    #is it a file or no?     #py test.txt
             raise IndexError(f'{argv[1]} is not file format... ')
 
-        with open(argv[1], 'r') as file:
+        with open(argv[1], 'r', encoding='utf-8') as file:
             lines = file.readlines()
             try:
                 mode = modes[argv[2]]
             except IndexError:
                 mode = 'line'
 
-                
             if mode == 'line':
                 print(len(lines))
 
@@ -28,10 +31,26 @@ def main():
                 print(len(n_word))
 
             elif mode == 'bytes':
-                n_byte = len(''.join(lines))
-                print(n_byte)
-        
-    except IndexError:
+                n_byte = len(''.join(lines).encode())
+                print(f'{n_byte} {argv[1]}')
+
+            elif mode == 'char':
+                char = 0
+                for words in lines:
+                    for ch in words:
+                        char += 1
+                print(f'{char} {argv[1]}')
+
+            elif mode == 'long line':
+                mx = len(lines[0])
+                counter = 1
+                while counter < len(lines):
+                    if len(lines[counter]) > mx:
+                        mx = len(lines[counter])
+                    counter += 1
+                print(f'{mx} {argv[1]}')
+
+    except IndexError:   # not file -> terminal mood
             lst = []
             while True:
                 text = input('Enter text: ')
